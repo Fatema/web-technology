57400f9ced445698cb59a80a4c08492e009e64ff
FAlkhanaizi <fatema.alkhanaizi@durham.ac.uk>
2018-12-05 20:42:30 +0000

    added code to score how close the titles are to the search term
    added more options to the user profile

--- API/recommendation_engine.py
+++ API/recommendation_engine.py
@@ -1,3 +1,5 @@
+from difflib import SequenceMatcher
+
 import pandas as pd
 import numpy as np
 from scipy.sparse.linalg import svds
@@ -8,6 +10,20 @@ class RecommendationEngine:
         self.movies = pd.read_csv('data/movies.csv')
         self.ratings = pd.read_csv('data/ratings.csv')
         self.update_movie_data()
+
+    def get_latest_user_id(self):
+        return self.ratings['userId'].max()
+
+    def get_movies(self, user_id, term=""):
+        user_full, recommendations = self.recommendation_ratings_matrix_factorization(user_id)
+        if recommendations is None and user_full is None:
+            movie_score = self.movies['title'].apply(lambda x: SequenceMatcher(None, term, x).ratio())
+            movies_list = (pd.merge(self.movies, movie_score.to_frame('close_score'), left_index=True, right_index=True).
+                           sort_values(by=['close_score']))
+            return movies_list
+        else:
+            recommendations['close_score'] = recommendations['title'].apply(lambda x: SequenceMatcher(None, term, x).ratio())
+            return recommendations
 
     def update_movie_data(self):
         self.movie_data = pd.merge(self.ratings, self.movies, on='movieId')
@@ -51,7 +67,7 @@ class RecommendationEngine:
 
         self.update_movie_data()
 
-    def recommendation_ratings_correlation(self, movie_id, num_recommendations=5):
+    def recommendation_ratings_correlation(self, movie_id):
         movie_ratings = self.user_movie_rating[movie_id]
 
         similar_movies = self.user_movie_rating.corrwith(movie_ratings)
@@ -63,17 +79,22 @@ class RecommendationEngine:
 
         # get top ten movies than have at least 50 ratings
         corr_movie_top10 = (corr_movie[corr_movie['rating_counts'] > 50].
-                                sort_values('Correlation', ascending=False).
-                                iloc[:num_recommendations, :-1])
+                                sort_values('Correlation', ascending=False))
 
         recommendations = pd.merge(self.movies, corr_movie_top10, how = 'inner')
 
         return recommendations
 
-    def recommendation_ratings_matrix_factorization(self, user_id, num_recommendations=5):
+    def recommendation_ratings_matrix_factorization(self, user_id):
         # Get and sort the user's predictions
-        user_row_number = user_id - 1  # UserId starts at 1, not 0
-        sorted_user_predictions = self.preds.iloc[user_row_number].sort_values(ascending=False)
+        # UserId starts at 1, not 0
+        # (in the table the ids start from 0 this is due to numpy array
+        # conversion and using the id as the index in the pivot table)
+        user_row_number = user_id - 1
+        if user_row_number in self.preds.index:
+            sorted_user_predictions = self.preds.iloc[user_row_number].sort_values(ascending=False)
+        else:
+            return None, None
 
         # Get the user's data and merge in the movie information.
         user_data = self.ratings[self.ratings.userId == user_id]
@@ -87,7 +108,6 @@ class RecommendationEngine:
                                      left_on='movieId',
                                      right_on='movieId').
                                rename(columns={user_row_number: 'Predictions'}).
-                               sort_values('Predictions', ascending=False).
-                               iloc[:num_recommendations, :-1])
+                               sort_values('Predictions', ascending=False))
 
         return user_full, recommendations
--- API/service.py
+++ API/service.py
@@ -7,16 +7,18 @@ from recommendation_engine import RecommendationEngine
 class Service:
 
     def __init__(self):
+        self.r_engine = RecommendationEngine()
+
         # all this information shouldn't be part of the api but for the sake of this assignment it is here
         with open('data/user_profile.json') as f:
             self.users = json.load(f)["profiles"]
+
         self.num_users = len(self.users)
-        self.last_id = max([user["user_id"] for user in self.users])
+        self.last_id = self.r_engine.get_latest_user_id()
+
         self.user_profile = None
 
         self.stop_words = set(stopwords.words('english'))
-
-        self.r_engine = RecommendationEngine()
 
     def has_profile(self, username):
         for user in self.users:
@@ -25,19 +27,26 @@ class Service:
                 return True
         return False
 
-    def add_new_profile(self, username):
+    def add_new_profile(self, username, genres):
         self.last_id += 1
         new_profile = {"user_id": self.last_id,
-                          "user_name": username,
-                          "search_words" : [],
-                          "search_genres" : [],
-                          "movie_ratings" : []
-                          }
+                       "user_name": username,
+                       "preferred_genres" : genres,
+                       "search_words" : [],
+                       "search_years" : [],
+                       "search_genres" : [],
+                       "search_ratings" : [],
+                       "movie_clicks" : [],
+                       "movie_ratings" : []
+                      }
         self.users[self.num_users] = new_profile
+        self.num_users += 1
+
         with open('data/user_profile.json', 'w') as f:
             json.dump({"profiles": self.users}, f)
 
         self.user_profile = new_profile
+
         return True
 
     def find_movies(self, search_term):
@@ -53,8 +62,12 @@ class Service:
             if not is_sw:
                 self.user_profile["search_words"].append({"word": t, "count": 1})
 
+        movies_list = self.r_engine.get_movies(self.user_profile["user_id"])
+
         # todo add call to r_engine to get the movie based on the search_term
         # make use of the user_profile as well if possible
+
+        # user the user profile to determine the order of the movies outputted
 
     def get_movies(self):
         if self.user_profile is None or \
